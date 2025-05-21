f5132195a5d6cde55a9b6d50cc5b70409239a11b
Bohan <bohan2001@gmail.com>
2025-05-21 22:01:20 +0800

    fix build error

--- fe_web/src/App.tsx
+++ fe_web/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // We'll update these imports after creating the TypeScript files
 import LandingPage from "./LandingPage";
